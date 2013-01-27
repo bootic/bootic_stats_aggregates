51fd4b4fee227e6799a34064959e75e678f82b07
Ismael Celis <ismaelct@gmail.com>
2013-01-27 15:46:00 +0000

    Links

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import(
   "log"
+  "fmt"
   "bootic_stats_observer/socket"
   "bootic_stats_observer/redis_stats"
   "net/http"
@@ -12,7 +13,44 @@ import(
   "strings"
 )
 
-func TimeSeriesHandler(client *redis.Client) (handle func(http.ResponseWriter, *http.Request)) {
+func AllKeysHandler(client *redis.Client) (handle func(http.ResponseWriter, *http.Request)) {
+  return func(res http.ResponseWriter, req *http.Request) {
+    res.Header().Add("Content-Type", "application/json")
+    res.Header().Add("Cache-Control", "no-store, no-cache, must-revalidate, private, proxy-revalidate")
+    res.Header().Add("Pragma", "no-cache")
+    res.Header().Add("Expires", "Fri, 24 Nov 2000 01:00:00 GMT")
+    
+    vars := mux.Vars(req)
+    splitPath := []string{vars["base"]}
+    if vars["key"] != "" {
+      splitPath = append(splitPath, vars["key"])
+    }
+    if vars["evt"] != "" {
+      splitPath = append(splitPath, vars["evt"])
+    }
+    
+    splitPath = append(splitPath, "*")
+    
+    redisPath := strings.Join(splitPath, ":")
+    f := client.Keys(redisPath)
+    
+    urls := []string{}
+    for _, v := range(f.Val()) {
+      url := fmt.Sprintf("http://%s/%s", req.Host, strings.Replace(v, ":", "/", -1))
+      urls = append(urls, url)
+    }
+    
+    json, err := json.Marshal(urls)
+    if err != nil {
+      panic(err)
+    }
+
+    res.Write(json)
+    
+  }
+}
+
+func KeyHandler(client *redis.Client) (handle func(http.ResponseWriter, *http.Request)) {
   return func(res http.ResponseWriter, req *http.Request) {
     res.Header().Add("Content-Type", "application/json")
     res.Header().Add("Cache-Control", "no-store, no-cache, must-revalidate, private, proxy-revalidate")
@@ -21,6 +59,7 @@ func TimeSeriesHandler(client *redis.Client) (handle func(http.ResponseWriter, *
     
     vars := mux.Vars(req)
     splitPath := []string{vars["base"], vars["key"]}
+    
     if vars["evt"] != "" {
       splitPath = append(splitPath, vars["evt"])
     }
@@ -92,10 +131,16 @@ func main() {
   http_host := "localhost:8001"
   
   router := mux.NewRouter()
-  handler := TimeSeriesHandler(tracker.Conn)
-  router.HandleFunc("/{base}/{key}/{evt}/{year}", handler).Methods("GET")
-  router.HandleFunc("/{base}/{key}/{evt}/{year}/{month}", handler).Methods("GET")
-  router.HandleFunc("/{base}/{key}/{evt}/{year}/{month}/{day}", handler).Methods("GET")
+  keyHandler := KeyHandler(tracker.Conn)
+  allKeysHandler := AllKeysHandler(tracker.Conn)
+  
+  router.HandleFunc("/{base}", allKeysHandler).Methods("GET")
+  router.HandleFunc("/{base}/{key}", allKeysHandler).Methods("GET")
+  router.HandleFunc("/{base}/{key}/{evt}", allKeysHandler).Methods("GET")
+  
+  router.HandleFunc("/{base}/{key}/{evt}/{year}", keyHandler).Methods("GET")
+  router.HandleFunc("/{base}/{key}/{evt}/{year}/{month}", keyHandler).Methods("GET")
+  router.HandleFunc("/{base}/{key}/{evt}/{year}/{month}/{day}", keyHandler).Methods("GET")
   
   http.Handle("/", router)
       
